f76866588213315c3544d92636a40bf130029ccd
mrance21 <mrance21@gmail.com>
2023-01-12 01:07:16 -0500

    added scenes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,18 @@
+import Navbar from "./scenes/Navbar"
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 
 function App() {
+  // this state determines what part of the navigation of the website we are at
+  const [selectedPage, setSelectedPage] = useState('home')
+  // this determines whether current browswer size min-width is less than or above 1060px
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+
   return (
-    <div className="App">
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
       
     </div>
   );
